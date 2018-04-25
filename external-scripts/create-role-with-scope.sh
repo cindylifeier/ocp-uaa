b484c26acb6aa44e18628f28609924a890c9c3cc
Shruti Rao <shruti.rao@feisystems.com>
2018-04-25 15:12:18 -0400

    Edit script to add more roles
    #19676:As a User I will have Benefits Specialist, Health Assistant, PCP and Front Office/Receptionist login

--- external-scripts/create-role-with-scope.sh
+++ external-scripts/create-role-with-scope.sh
@@ -3,10 +3,15 @@ function showOptions()
 {
 echo -e "What role do you want to create today?"
 echo -e "1. OCP Admin"
-echo -e "2. Case Manager/ Care Coordinator"
-echo -e "3. Care Manager"
-echo -e "4. Organization Admin"
-echo -e "5. Patient"
+echo -e "2. Patient"
+echo -e "3. Case Manager / Care Coordinator"
+echo -e "4. Care Manager"
+echo -e "5. Organization Admin"
+echo -e "6. Primary Care Provider"
+echo -e "7. Benefits Specialist"
+echo -e "8. Health Assistant"
+echo -e "9. Front Office Receptionist"
+
 echo -e "Enter your choice :"
 read choice
 }
@@ -81,6 +86,14 @@ orgAdminScope=(ocpUi.access ocpUiApi.location_create ocpUiApi.location_read ocpU
 
 patientScope=(ocpUi.access ocpUiApi.activityDefinition_read ocpUiApi.appointment_read ocpUiApi.appointment_update ocpUiApi.careTeam_read ocpUiApi.careTeam_update ocpUiApi.careTeam_delete ocpUiApi.consent_create ocpUiApi.consent_read ocpUiApi.consent_update ocpUiApi.consent_delete ocpUiApi.relatedPerson_read ocpUiApi.relatedPerson_update ocpUiApi.appointment_read ocpUiApi.appointment_update ocpUiApi.relatedPerson_create ocpUiApi.relatedPerson_read ocpUiApi.relatedPerson_update ocpUiApi.relatedPerson_delete ocpUiApi.task_create ocpUiApi.task_read ocpUiApi.task_update ocpUiApi.task_delete)
 
+pcpScope=(ocpUi.access ocpUiApi.activityDefinition_read ocpUiApi.task_create ocpUiApi.task_read ocpUiApi.task_update ocpUiApi.task_delete ocpUiApi.patient_read ocpUiApi.careTeam_read ocpUiApi.appointment_read ocpUiApi.consent_create ocpUiApi.consent_read ocpUiApi.consent_update ocpUiApi.consent_delete ocpUiApi.communication_create ocpUiApi.communication_read ocpUiApi.communication_update ocpUiApi.communication_delete ocpUiApi.practitioner_read ocpUiApi.relatedPerson_read)
+
+benefitsSpecialistScope=(ocpUi.access ocpUiApi.activityDefinition_read ocpUiApi.task_create ocpUiApi.task_read ocpUiApi.task_update ocpUiApi.task_delete ocpUiApi.patient_read ocpUiApi.careTeam_read ocpUiApi.appointment_create ocpUiApi.appointment_read ocpUiApi.appointment_update ocpUiApi.appointment_delete ocpUiApi.consent_create ocpUiApi.consent_read ocpUiApi.consent_update ocpUiApi.consent_delete ocpUiApi.communication_create ocpUiApi.communication_read ocpUiApi.communication_update ocpUiApi.communication_delete ocpUiApi.practitioner_read ocpUiApi.relatedPerson_read)
+
+healthAssistantScope=(ocpUi.access ocpUiApi.activityDefinition_read ocpUiApi.task_read ocpUiApi.patient_read ocpUiApi.careTeam_read ocpUiApi.appointment_create ocpUiApi.appointment_read ocpUiApi.appointment_update ocpUiApi.appointment_delete ocpUiApi.consent_read ocpUiApi.communication_create ocpUiApi.communication_read ocpUiApi.communication_update ocpUiApi.communication_delete ocpUiApi.practitioner_read ocpUiApi.relatedPerson_read)
+
+frontOfficeReceptionistScope=(ocpUi.access ocpUiApi.activityDefinition_read ocpUiApi.task_read ocpUiApi.patient_read ocpUiApi.appointment_create ocpUiApi.appointment_read ocpUiApi.appointment_update ocpUiApi.appointment_delete ocpUiApi.communication_create ocpUiApi.communication_read ocpUiApi.communication_update ocpUiApi.communication_delete ocpUiApi.practitioner_read ocpUiApi.relatedPerson_read)
+
 case "$4" in
 		1)
 			echo -e "Adding scopes to $1"
@@ -97,6 +110,19 @@ case "$4" in
 			;;
 		2)
 			echo -e "Adding scopes to $1"
+			echo "Number of scopes to be added: ${#patientScope[*]}"
+			for index in ${!patientScope[*]}
+			do
+				groupId=`getGroupId "${patientScope[$index]}" "${bearerToken}"`
+				echo -e "\n Got Group Id for ${patientScope[$index]}"
+				createGroupMembership "${groupId}" "${roleId}" "${bearerToken}"
+				echo -e "\n Created membership between ${patientScope[$index]} and ${1}"
+				echo -e "\n Done: $((index+1)) of ${#patientScope[*]}"
+			done
+			break
+			;;			
+		3)
+			echo -e "Adding scopes to $1"
 			echo "Number of scopes to be added: ${#careCoordinatorScope[*]}"
 			for index in ${!careCoordinatorScope[*]}
 			do
@@ -108,7 +134,7 @@ case "$4" in
 			done
 			break
 			;;
-		3)
+		4)
 			echo -e "Adding scopes to $1"
 			echo "Number of scopes to be added: ${#careManagerScope[*]}"
 			for index in ${!careManagerScope[*]}
@@ -121,7 +147,7 @@ case "$4" in
 			done
 			break
 			;;
-		4)
+		5)
 			echo -e "Adding scopes to $1"
 			echo "Number of scopes to be added: ${#orgAdminScope[*]}"
 			for index in ${!orgAdminScope[*]}
@@ -134,19 +160,58 @@ case "$4" in
 			done
 			break
 			;;	
-		5)
+		6)
 			echo -e "Adding scopes to $1"
-			echo "Number of scopes to be added: ${#patientScope[*]}"
-			for index in ${!patientScope[*]}
+			echo "Number of scopes to be added: ${#pcpScope[*]}"
+			for index in ${!pcpScope[*]}
 			do
-				groupId=`getGroupId "${patientScope[$index]}" "${bearerToken}"`
-				echo -e "\n Got Group Id for ${patientScope[$index]}"
+				groupId=`getGroupId "${pcpScope[$index]}" "${bearerToken}"`
+				echo -e "\n Got Group Id for ${pcpScope[$index]}"
 				createGroupMembership "${groupId}" "${roleId}" "${bearerToken}"
-				echo -e "\n Created membership between ${patientScope[$index]} and ${1}"
-				echo -e "\n Done: $((index+1)) of ${#patientScope[*]}"
+				echo -e "\n Created membership between ${pcpScope[$index]} and ${1}"
+				echo -e "\n Done: $((index+1)) of ${#pcpScope[*]}"
 			done
 			break
 			;;
+		7)
+			echo -e "Adding scopes to $1"
+			echo "Number of scopes to be added: ${#benefitsSpecialistScope[*]}"
+			for index in ${!benefitsSpecialistScope[*]}
+			do
+				groupId=`getGroupId "${benefitsSpecialistScope[$index]}" "${bearerToken}"`
+				echo -e "\n Got Group Id for ${benefitsSpecialistScope[$index]}"
+				createGroupMembership "${groupId}" "${roleId}" "${bearerToken}"
+				echo -e "\n Created membership between ${benefitsSpecialistScope[$index]} and ${1}"
+				echo -e "\n Done: $((index+1)) of ${#benefitsSpecialistScope[*]}"
+			done
+			break
+			;;
+		8)
+			echo -e "Adding scopes to $1"
+			echo "Number of scopes to be added: ${#healthAssistantScope[*]}"
+			for index in ${!healthAssistantScope[*]}
+			do
+				groupId=`getGroupId "${healthAssistantScope[$index]}" "${bearerToken}"`
+				echo -e "\n Got Group Id for ${healthAssistantScope[$index]}"
+				createGroupMembership "${groupId}" "${roleId}" "${bearerToken}"
+				echo -e "\n Created membership between ${healthAssistantScope[$index]} and ${1}"
+				echo -e "\n Done: $((index+1)) of ${#healthAssistantScope[*]}"
+			done
+			break
+			;;
+		9)
+			echo -e "Adding scopes to $1"
+			echo "Number of scopes to be added: ${#frontOfficeReceptionistScope[*]}"
+			for index in ${!frontOfficeReceptionistScope[*]}
+			do
+				groupId=`getGroupId "${frontOfficeReceptionistScope[$index]}" "${bearerToken}"`
+				echo -e "\n Got Group Id for ${frontOfficeReceptionistScope[$index]}"
+				createGroupMembership "${groupId}" "${roleId}" "${bearerToken}"
+				echo -e "\n Created membership between ${frontOfficeReceptionistScope[$index]} and ${1}"
+				echo -e "\n Done: $((index+1)) of ${#frontOfficeReceptionistScope[*]}"
+			done
+			break
+			;;	
 		*)
 			;;	
 		esac
@@ -178,6 +243,16 @@ while [ choice != '' ]
 			break
 			;;
 		2)
+			echo -e "You have chosen to create a Patient"
+			echo -e "What Organization ID does this Patient belong to?"
+			read org
+			role="ocp.role.organization.${org}.patient";
+			description="Role Patient for Organization : ${org}";
+			echo -e "The new role created will be: ${role}"
+			createRoleAndAddScopes "${role}" "${description}" "${bearerToken}" "${choice}"
+			break
+			;;	
+		3)
 			echo -e "You have chosen to create a Case Manager/ Care Coordinator"
 			echo -e "What Organization ID does this Case Manager/ Care Coordinator belong to?"
 			read org
@@ -187,7 +262,7 @@ while [ choice != '' ]
 			createRoleAndAddScopes "${role}" "${description}" "${bearerToken}" "${choice}"
 			break
 			;;
-		3)
+		4)
 			echo -e "You have chosen to create a Care Manager"
 			echo -e "What Organization ID does this Care Manager belong to?"
 			read org
@@ -197,7 +272,7 @@ while [ choice != '' ]
 			createRoleAndAddScopes "${role}" "${description}" "${bearerToken}" "${choice}"
 			break
 			;;
-		4)
+		5)
 			echo -e "You have chosen to create a Organization Admin"
 			echo -e "What Organization ID does this Organization Admin belong to?"
 			read org
@@ -207,12 +282,42 @@ while [ choice != '' ]
 			createRoleAndAddScopes "${role}" "${description}" "${bearerToken}" "${choice}"
 			break
 			;;
-		5)
-			echo -e "You have chosen to create a Patient"
-			echo -e "What Organization ID does this Patient belong to?"
+		6)
+			echo -e "You have chosen to create a Primary Care Provider"
+			echo -e "What Organization ID does this Primary Care Provider belong to?"
 			read org
-			role="ocp.role.organization.${org}.patient";
-			description="Role Patient for Organization : ${org}";
+			role="ocp.role.organization.${org}.primaryCareProvider";
+			description="Primary Care Provider for Organization : ${org}";
+			echo -e "The new role created will be: ${role}"
+			createRoleAndAddScopes "${role}" "${description}" "${bearerToken}" "${choice}"
+			break
+			;;
+		7)
+			echo -e "You have chosen to create a Benefits Specialist"
+			echo -e "What Organization ID does this Benefits Specialist belong to?"
+			read org
+			role="ocp.role.organization.${org}.benefitsSpecialist";
+			description="Benefits Specialist for Organization : ${org}";
+			echo -e "The new role created will be: ${role}"
+			createRoleAndAddScopes "${role}" "${description}" "${bearerToken}" "${choice}"
+			break
+			;;
+		8)
+			echo -e "You have chosen to create a Health Assistant"
+			echo -e "What Organization ID does this Health Assistant belong to?"
+			read org
+			role="ocp.role.organization.${org}.healthAssistant";
+			description="Health Assistant for Organization : ${org}";
+			echo -e "The new role created will be: ${role}"
+			createRoleAndAddScopes "${role}" "${description}" "${bearerToken}" "${choice}"
+			break
+			;;
+		9)
+			echo -e "You have chosen to create a Front Office Receptionist"
+			echo -e "What Organization ID does this Front Office Receptionist belong to?"
+			read org
+			role="ocp.role.organization.${org}.frontOfficeReceptionist";
+			description="Front Office Receptionist for Organization : ${org}";
 			echo -e "The new role created will be: ${role}"
 			createRoleAndAddScopes "${role}" "${description}" "${bearerToken}" "${choice}"
 			break
